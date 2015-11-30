7522e2b24839d82ff3b5041a224e0ac8de733fac
RoyChan <690374857@qq.com>
2015-11-30 17:44:39 +0800

    Initial commit

---
+++ web/src/main/java/com/viewmap/controller/defaultPageController.java
@@ -0,0 +1,24 @@
+package com.viewmap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Administrator on 2015/11/27.
+ */
+@Controller
+public class defaultPageController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(Model modle){
+        return "hello";
+    }
+
+
+    @RequestMapping(value = "/error",method = RequestMethod.GET)
+    public String error(Model modle){
+        return "error";
+    }
+}
