9336359df2e6268aeef730413a26dd1db7d91697
RoyChan <690374857@qq.com>
2015-12-01 17:21:37 +0800

    更改servlet的上下文

--- web/src/main/java/com/viewmap/controller/defaultPageController.java
+++ web/src/main/java/com/viewmap/controller/defaultPageController.java
@@ -8,17 +8,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Created by Administrator on 2015/11/27.
  */
+//用于指向welcome和error
 @Controller
 public class defaultPageController {
 
-//    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-//    public String hello(Model modle){
-//        return "hello";
-//    }
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(Model modle){
+        return "hello";
+    }
 
 
-//    @RequestMapping(value = "/error",method = RequestMethod.GET)
-//    public String error(Model modle){
-//        return "error";
-//    }
+    @RequestMapping(value = "/error",method = RequestMethod.GET)
+    public String error(Model modle){
+        return "error";
+    }
 }
