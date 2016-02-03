3740fede0a1621ff2bf37cba2c3e9fa6457523ef
RoyChan <690374857@qq.com>
2016-02-03 18:30:44 +0800

    加入json测试页面

--- service/src/main/java/com/transation/service/impl/DemooServiceImpl.java
+++ service/src/main/java/com/transation/service/impl/DemooServiceImpl.java
@@ -1,6 +1,7 @@
 package com.transation.service.impl;
 
 import com.transation.dao.DemoDAO;
+import com.transation.model.Fruit;
 import com.transation.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
