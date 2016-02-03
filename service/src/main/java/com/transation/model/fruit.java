7bc4eee29910a02529e5ae47fb1d224086219677
RoyChan <690374857@qq.com>
2016-02-03 19:02:55 +0800

    加入DAO使用demo

--- service/src/main/java/com/transation/model/fruit.java
+++ service/src/main/java/com/transation/model/fruit.java
@@ -1,57 +1,34 @@
 package com.transation.model;
 
-import java.io.Serializable;
-
 /**
- * Created by Administrator on 2015/12/2.
+ * Created by Administrator on 2016/2/3.
  */
-public class fruit implements Serializable{
-    private final String name;
-    private final String price;
-    private final String country;
+public class fruit {
+    private String name;
+    private String price;
+    private String country;
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getPrice() {
         return price;
     }
 
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
     public String getCountry() {
         return country;
     }
 
-    private fruit(buildfruit bf){
-        this.name = bf.name;
-        this.price = bf.price;
-        this.country = bf.country;
+    public void setCountry(String country) {
+        this.country = country;
     }
-
-
-    public static class buildfruit{
-        private String name = null;
-        private String price = null;
-        private String country = null;
-
-        public buildfruit name(String name){
-            this.name = name;
-            return this;
-        }
-
-        public buildfruit price(String price){
-            this.price = price;
-            return this;
-        }
-
-        public buildfruit country(String country){
-            this.country = country;
-            return this;
-        }
-
-        public fruit dobuild(){
-            return new fruit(this);
-        }
-    }
-
 }
